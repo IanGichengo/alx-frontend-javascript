399b50ed97539c9033610cd75873dc233952c0ab
IanGichengo <iangatimu6@gmail.com>
2024-07-24 16:04:00 -0400

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,6 @@
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
         // Simulating an asynchronous operation
-        setTimeout(() => {
-            resolve("Success");
-        }, 1000);
+      resolve();
     });
 }
