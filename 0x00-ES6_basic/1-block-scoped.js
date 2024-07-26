09f130000640a6f529cd36bd43a2c86f40c96b96
IanGichengo <iangatimu6@gmail.com>
2024-07-26 15:21:51 -0400

    es6_basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 /* modified so that vars aren't overwritten */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const taskInBlock = true;
-    const task2InBlock = false;
+    const task = true;
+    const task = false;
   }
 
   return [task, task2];
