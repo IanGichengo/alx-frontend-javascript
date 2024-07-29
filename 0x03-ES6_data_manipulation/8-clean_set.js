f3abf957f898a8f78dc6fc2f15361b81b065a779
IanGichengo <iangatimu6@gmail.com>
2024-07-29 16:45:16 -0400

    es6_data-manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set || startString.length === 0) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
-  return Array.from(set).filter((item) => item.startsWith(startString)).map((item) => item.slice(startString.length)).join('-');
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
